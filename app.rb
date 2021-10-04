735d9746d583d273e5589d62b1ba7a92c7e7d294
Philip Carr <philipcarr@Philips-MacBook-Air.local>
2021-10-04 15:23:43 +0100

    defining some new routes

--- app.rb
+++ app.rb
@@ -1,5 +1,18 @@
 require 'sinatra'
+require "sinatra/reloader" if development?
 
 get '/' do
-  "hello!"
+  "Hello World"
+end
+
+get '/secret' do
+  "Don't tell me"
+end
+
+get '/message' do
+  "This is a message"
+end
+
+get '/test' do
+  "This is a test"
 end
